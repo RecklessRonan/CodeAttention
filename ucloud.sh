81ea29ee5a6a2d060528d710399bd7a2d580f5ca
recklessronan <380971080@qq.com>
2021-12-13 09:27:59 +0800

    update

---
+++ ucloud.sh
@@ -0,0 +1,6 @@
+bash run.sh roberta summarize python
+bash run.sh codebert summarize python
+bash run.sh graphcodebert summarize python
+bash run.sh t5 summarize python
+bash run.sh codet5 summarize python
+bash run.sh bart summarize python
