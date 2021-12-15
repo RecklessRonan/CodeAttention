2003e93fea98052347e0116609825476e6aa7e5d
recklessronan <380971080@qq.com>
2021-12-15 08:01:16 +0000

    update

--- 13.sh
+++ 13.sh
@@ -1,6 +1,1 @@
-bash run.sh roberta summarize php
-bash run.sh codebert summarize php
-bash run.sh graphcodebert summarize php
 bash run.sh t5 summarize php
-bash run.sh codet5 summarize php
-bash run.sh bart summarize php
